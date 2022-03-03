0010ab22fc05d7700c989d2cbaba927a2340ae7e
Carlos Felipe Gonzalez Galvis <carlosfelipegg28@gmail.com>
2022-03-03 03:55:13 -0500

    upload pdf (#17)

--- app/models/file_attachment.rb
+++ app/models/file_attachment.rb
@@ -1,7 +1,12 @@
 class FileAttachment < ApplicationRecord
+	require 'base64'
 	self.table_name = 'file'
 	# Model associations
 
 	# Signature - User
 	has_many :user, foreign_key: :signature_id
+
+	def get_base_64
+		Base64.encode64(self.file.each_byte{|byte| byte})
+	end
 end
